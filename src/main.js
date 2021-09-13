646fabd799573d4f8636049fd15d32428f9f569f
duxinwu521 <13935741214@163.com>
2021-09-13 22:05:06 +0800

    添加console

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
+/*
+ * @Descripttion:
+ * @TapdLink: https://XXX
+ * @DesignsLink: https://XXX
+ * @RelatedPersons: XXX[市场]、XXX[产品]、XXX[前端]、XXX[后端]、XXX[设计]
+ * @version: 1.0.0
+ * @Author: duxinwu
+ * @Date: 2021-09-13 19:17:25
+ * @LastEditors: duxinwu
+ * @LastEditTime: 2021-09-13 22:03:58
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 createApp(App).use(store).use(router).mount('#app')
+console.log(11111)
